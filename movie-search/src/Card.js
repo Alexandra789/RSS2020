47b8e6b9f8d28d92e2f87ff234ad0ed7e4875960
unknown <zakovinko99@gmail.com>
2020-05-10 17:26:03 +0300

    feat : add adaptive layout

--- movie-search/src/Card.js
+++ movie-search/src/Card.js
@@ -1,22 +1,25 @@
+const TITLE_URL = 'https://www.imdb.com/title/';
+
 export default class Card {
   constructor(data) {
     this.title = data.Title;
     this.poster = data.Poster;
     this.year = data.Year;
+    this.id = data.imdbID;
     this.rating = 0;
   }
 
   render() {
     const element = document.createElement('div');
     element.classList.add('card');
-    element.classList.add('col-xs-6');
+    element.classList.add('carousel-cell');
     element.innerHTML = `
-        <img class="card-img-top" src="${this.poster}" alt="movie poster"/>
-        <div class="card-body">
-            <h5 class="card-title">${this.title}</h5>
-            <p>${this.year}</p>
+            <img class="card-img-top" src="${this.poster}" alt="movie poster"/>
+            <div class="card-body">
+                <a class="card-title" href="${TITLE_URL}${this.id}">${this.title}</a>
+                <p>${this.year}</p>
 <!--                <br><i class="fas fa-star">-->
-        </div>`;
+            </div>`;
     return element;
   }
 }
