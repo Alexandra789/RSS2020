d3c598c3d25d9cb2889b76704799dd4174f47bb0
unknown <zakovinko99@gmail.com>
2020-05-10 20:16:59 +0300

    feat : add rating

--- movie-search/src/ApiManager.js
+++ movie-search/src/ApiManager.js
@@ -1,31 +1,45 @@
 const API_URL = 'https://www.omdbapi.com';
 const API_KEY = '3fd78803';
-// const TITLES = ['batman', 'harry potter', 'star wars', 'lord of the rings'];
+const TITLES = ['batman', 'harry potter', 'star wars', 'tokyo'];
+
+function getRatings(data) {
+  return Promise.all(data.map((entry) => {
+    const url = `${API_URL}/?i=${entry.imdbID}&apikey=${API_KEY}`;
+    return fetch(url).then((res) => res.json());
+  }));
+}
+
+function getRandomTitle() {
+  return TITLES[Math.floor(Math.random() * TITLES.length)];
+}
 
 export default class ApiManager {
   constructor(layout) {
     this.layout = layout;
-    this.search('batman');
+    this.search(getRandomTitle());
   }
 
-  async search(query) {
+  search(query) {
     const url = `${API_URL}/?s=${query}&apikey=${API_KEY}&type=movie`;
     this.layout.toggleLoading(true);
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      if (!data.Search) {
-        this.layout.showAlert('No results found. Try something else');
-      } else {
-        this.layout.toggleLoading(false);
-        this.layout.createMovieCards(data.Search);
-      }
-    } catch (err) {
-      this.layout.showAlert(err);
-    }
+    return fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.Search) {
+          this.layout.showAlert('No results found. Try something else');
+        } else {
+          const result = data.Search;
+          getRatings(result).then((res) => {
+            for (let i = 0; i < res.length; i += 1) {
+              result[i].rating = res[i].imdbRating;
+            }
+            this.layout.toggleLoading(false);
+            this.layout.createMovieCards(result);
+          });
+        }
+      })
+      .catch((err) => {
+        this.layout.showAlert(err);
+      });
   }
-
-  // getRandomTitle() {
-  //   return TITLES[Math.floor(Math.random() * TITLES.length)];
-  // }
 }
--- movie-search/src/Card.js
+++ movie-search/src/Card.js
@@ -6,7 +6,7 @@ export default class Card {
     this.poster = data.Poster;
     this.year = data.Year;
     this.id = data.imdbID;
-    this.rating = 0;
+    this.rating = data.rating;
   }
 
   render() {
@@ -14,12 +14,12 @@ export default class Card {
     element.classList.add('card');
     element.classList.add('carousel-cell');
     element.innerHTML = `
-            <img class="card-img-top" src="${this.poster}" alt="movie poster"/>
-            <div class="card-body">
-                <a class="card-title" href="${TITLE_URL}${this.id}">${this.title}</a>
-                <p>${this.year}</p>
-<!--                <br><i class="fas fa-star">-->
-            </div>`;
+        <img class="card-img-top" src="${this.poster}" alt="movie poster"
+        onerror="this.onerror=null; this.src='assets/images/no-poster.png'"/>
+        <div class="card-body">
+            <a class="card-title" href="${TITLE_URL}${this.id}">${this.title}</a>
+            <p class="card-info">${this.year} <span class="card-rating">&#128970;${this.rating}</span></p>
+        </div>`;
     return element;
   }
 }
